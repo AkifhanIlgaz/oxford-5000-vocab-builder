334b2d56d7c866080fcde3c0e6b976b228f23efa
AkifhanIlgaz <akifhanilgaz@gmail.com>
2023-07-10 16:49:30 +0300

    create temporary endpoint for testing

--- main.go
+++ main.go
@@ -1,5 +1,30 @@
 package main
 
-func main() {
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
 
+	"github.com/AkifhanIlgaz/vocab-builder/parser"
+	"github.com/go-chi/chi/v5"
+)
+
+const baseUrl = "https://www.oxfordlearnersdictionaries.com/definition/english/"
+
+func main() {
+	router := chi.NewRouter()
+
+	router.Get("/{word}", func(w http.ResponseWriter, r *http.Request) {
+		wordInfo, err := parser.ParseWord(baseUrl + chi.URLParam(r, "word"))
+		if err != nil {
+			http.Error(w, "Something went wrong", http.StatusInternalServerError)
+		}
+
+		x, _ := json.MarshalIndent(wordInfo, "", "\t")
+
+		w.Write(x)
+	})
+
+	fmt.Println("Serving")
+	http.ListenAndServe(":3000", router)
 }
