109f42879e6114ed4220088c8e8c287156fc8b79
AkifhanIlgaz <akifhanilgaz@gmail.com>
2023-07-11 17:09:52 +0300

    we won't use wordPackage logic

--- cmd/exp/exp.go
+++ cmd/exp/exp.go
@@ -4,15 +4,8 @@ import (
 	"context"
 	"os"
 
-	"github.com/AkifhanIlgaz/vocab-builder/helper"
 	"github.com/AkifhanIlgaz/vocab-builder/models"
 	"github.com/joho/godotenv"
-)
-
-const (
-	withIdioms    = "https://www.oxfordlearnersdictionaries.com/definition/english/about_2"
-	withoutIdioms = "https://www.oxfordlearnersdictionaries.com/definition/english/across_2"
-	diff          = "https://www.oxfordlearnersdictionaries.com/definition/english/reject_1"
 )
 
 func main() {
@@ -29,10 +22,9 @@ func main() {
 		}
 	}()
 
-	wordsCollection := client.Database("Vocab-Builder").Collection("Words")
+	// wordsCollection := client.Database("Vocab-Builder").Collection("Words")
 	// wordService := models.WordService{
 	// 	WordCollection: wordsCollection,
 	// }
 
-	helper.RenameBoxField(wordsCollection)
 }
--- models/words.go
+++ models/words.go
@@ -34,30 +34,3 @@ func (service *WordService) BoxLevelUp(wordId int) error {
 func (service *WordService) BoxLevelDown(wordId int) error {
 	panic("")
 }
-
-// wordsPerDay will be stored on User table or as cookie ?
-func (service *WordService) GetWordPackage(packageId int, wordsPerDay int) []*WordInfo {
-	var words []*WordInfo
-
-	for _, wordId := range service.getWordIds(packageId, wordsPerDay) {
-		word, err := service.GetWord(wordId)
-		if err != nil {
-			return nil
-		}
-		words = append(words, word)
-	}
-
-	return words
-}
-
-// packageId will be 1-indexed
-// [wordsPerDay * (packageId -1), wordsPerDay * packageId)
-func (service *WordService) getWordIds(packageId int, wordsPerDay int) []int {
-	wordIds := []int{}
-
-	for i := wordsPerDay * (packageId - 1); i < wordsPerDay*(packageId); i++ {
-		wordIds = append(wordIds, i)
-	}
-
-	return wordIds
-}
