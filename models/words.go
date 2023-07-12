d6572d98097dc7eab2ba707373cbdc08da39098d
AkifhanIlgaz <akifhanilgaz@gmail.com>
2023-07-12 15:15:30 +0300

    move opening database functions to database module

--- models/words.go
+++ models/words.go
@@ -8,6 +8,34 @@ import (
 	"go.mongodb.org/mongo-driver/mongo"
 )
 
+type WordInfo struct {
+	Id          int          `json:"id"`
+	Box         int          `json:"box"`
+	Word        string       `json:"word"`
+	Header      Header       `json:"header"`
+	Definitions []Definition `json:"definitions"`
+	Idioms      []Idiom      `json:"idioms"`
+}
+
+type Header struct {
+	Audio struct {
+		UK string `json:"UK"`
+		US string `json:"US"`
+	} `json:"audio"`
+	PartOfSpeech string `json:"partOfSpeech"`
+	CEFRLevel    string `json:"CEFRLevel"`
+}
+
+type Definition struct {
+	Meaning  string   `json:"meaning"`
+	Examples []string `json:"examples"`
+}
+
+type Idiom struct {
+	Usage       string       `json:"usage"`
+	Definitions []Definition `json:"definition"`
+}
+
 type WordService struct {
 	WordCollection *mongo.Collection
 }
