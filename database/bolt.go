475db44618cff1842742ba0e4f242309da33cf83
AkifhanIlgaz <akifhanilgaz@gmail.com>
2023-07-12 15:40:04 +0300

    load env

--- database/bolt.go
+++ database/bolt.go
@@ -8,11 +8,11 @@ import (
 )
 
 type BoltConfig struct {
-	FileName string
+	Path string
 }
 
 func OpenBolt(config BoltConfig) (*bolt.DB, error) {
-	db, err := bolt.Open(config.FileName, 0600, &bolt.Options{Timeout: 1 * time.Second})
+	db, err := bolt.Open(config.Path, 0600, &bolt.Options{Timeout: 1 * time.Second})
 	if err != nil {
 		return nil, fmt.Errorf("open bolt: %w", err)
 	}
--- main.go
+++ main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"fmt"
+	"os"
+	"path/filepath"
 
 	"github.com/AkifhanIlgaz/vocab-builder/database"
 	_ "github.com/jackc/pgx/v4/stdlib"
@@ -12,7 +14,7 @@ import (
 type config struct {
 	Postgres database.PostgresConfig
 	Mongo    database.MongoConfig
-	Bolt     database.BoltConfig // Use home dir
+	Bolt     database.BoltConfig // UserHomeDir + FileName
 	Server   struct {
 		Address string
 	}
@@ -25,7 +27,38 @@ func loadEnvConfig() (config, error) {
 		return cfg, fmt.Errorf("load env: %w", err)
 	}
 
-	return config{}, nil
+	cfg.Postgres = database.PostgresConfig{
+		Host:     os.Getenv("POSTGRES_HOST"),
+		Port:     os.Getenv("POSTGRES_PORT"),
+		User:     os.Getenv("POSTGRES_USER"),
+		Password: os.Getenv("POSTGRES_PASSWORD"),
+		DBName:   os.Getenv("POSTGRES_DBNAME"),
+		SSLMode:  os.Getenv("POSTGRES_SSLMODE"),
+	}
+	if cfg.Postgres.Host == "" && cfg.Postgres.Port == "" {
+		return cfg, fmt.Errorf("no Postgres config provided")
+	}
+
+	cfg.Mongo = database.MongoConfig{
+		ConnectionURI: os.Getenv("MONGODB_URI"),
+	}
+	if cfg.Mongo.ConnectionURI == "" {
+		return cfg, fmt.Errorf("no Mongo config provided")
+	}
+
+	boltFileName := os.Getenv("BOLT_FILENAME")
+	if boltFileName == "" {
+		return cfg, fmt.Errorf("no BoltDB config provided")
+	}
+	home, err := os.UserHomeDir()
+	if err != nil {
+		return cfg, fmt.Errorf("load env: %w", err)
+	}
+	cfg.Bolt.Path = filepath.Join(home, boltFileName)
+
+	cfg.Server.Address = os.Getenv("SERVER_ADDRESS")
+
+	return cfg, nil
 }
 
 func main() {
