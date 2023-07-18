1ed2e8082c8d7cc5653c5e31d4b650d58f088d84
AkifhanIlgaz <akifhanilgaz@gmail.com>
2023-07-18 22:32:03 +0300

    add function signatures to box controller

--- controllers/boxes.go
+++ controllers/boxes.go
@@ -11,12 +11,23 @@ type BoxController struct {
 	// TODO: Add other services if necessary
 }
 
-func (bc *BoxController) GetTodaysWords(w http.ResponseWriter, r *http.Request) {
-	
-	w.Write([]byte("Get todays words"))
+func (bc *BoxController) NewWordBox(w http.ResponseWriter, r *http.Request) {
 
+}
+
+func (bc *BoxController) GetTodaysWords(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Get todays words"))
 }
 
 func (bc *BoxController) GetWordByLevel(w http.ResponseWriter, r *http.Request) {
 	// TODO: Extract user from request's context
+}
+
+// TODO: Change function names. There is duplicate functions on different modules
+func (bc *BoxController) LevelUp(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func (bc *BoxController) LevelDown(w http.ResponseWriter, r *http.Request) {
+
 }
