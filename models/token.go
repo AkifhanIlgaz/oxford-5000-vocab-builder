30068a166c4316edd321c4033c6e7e71202f23dc
AkifhanIlgaz <akifhanilgaz@gmail.com>
2023-10-15 21:56:51 +0300

    create function signatures for tokens

--- models/token.go
+++ models/token.go
@@ -12,10 +12,12 @@ type TokenService struct {
 	Collection *mongo.Collection
 }
 
-func NewTokenService(client *mongo.Client) TokenService {
+func NewTokenService(client *mongo.Client) *TokenService {
 	collection := client.Database(Database).Collection(RefreshTokenCollection)
 
-	
+	return &TokenService{
+		Collection: collection,
+	}
 }
 
 type UserClaims struct {
@@ -23,7 +25,7 @@ type UserClaims struct {
 	jwt.RegisteredClaims
 }
 
-func NewIdToken(uid string) (string, error) {
+func (service *TokenService) NewIdToken(uid string) (string, error) {
 	token := jwt.NewWithClaims(jwt.SigningMethodHS256, UserClaims{
 		Uid: uid,
 		RegisteredClaims: jwt.RegisteredClaims{
@@ -38,4 +40,37 @@ func NewIdToken(uid string) (string, error) {
 	}
 
 	return t, nil
+}
+
+// TODO: Create Refresh Token for user
+/*
+	!Errors
+		User doesn't exist
+
+*/
+func (service *TokenService) NewRefreshToken(uid string) (string, error) {
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, UserClaims{
+		Uid: uid,
+		RegisteredClaims: jwt.RegisteredClaims{
+			IssuedAt:  jwt.NewNumericDate(time.Now()),
+			ExpiresAt: jwt.NewNumericDate(time.Now().Add(ExpireDuration)),
+		},
+	})
+
+	t, err := token.SignedString(Secret)
+	if err != nil {
+		return "", fmt.Errorf("new id token: %w", err)
+	}
+
+	return t, nil
+}
+
+/*
+!Errors
+User doesn't exist
+Refresh token expired && not valid => refresh token isn't same as the refresh token on DB
+*/
+func (service *TokenService) RefreshIdToken(uid, refreshToken string) (string, error) {
+
+	panic("Implement")
 }
