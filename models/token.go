f2e487148e95d6ce52568c905f94dd3da99b0df1
AkifhanIlgaz <akifhanilgaz@gmail.com>
2023-10-16 10:12:32 +0300

    check if users exists by object id

--- models/token.go
+++ models/token.go
@@ -1,26 +1,28 @@
 package models
 
 import (
+	"context"
 	"fmt"
 	"time"
 
+	"github.com/AkifhanIlgaz/vocab-builder/rand"
 	"github.com/golang-jwt/jwt/v5"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
 )
 
 type TokenService struct {
-	UsersCollection            *mongo.Collection
-	RefreshTokenCollection     *mongo.Collection
-	idTokenExpireDuration      time.Duration
-	refreshTokenExpireDuration time.Duration
+	UsersCollection        *mongo.Collection
+	RefreshTokenCollection *mongo.Collection
+	idTokenExpireDuration  time.Duration
 }
 
-func NewTokenService(client *mongo.Client, idTokenExpireDuration, refreshTokenExpireDuration time.Duration) *TokenService {
-	return &TokenService{
-		UsersCollection:            getCollection(client, UsersCollection),
-		RefreshTokenCollection:     getCollection(client, RefreshTokenCollection),
-		idTokenExpireDuration:      idTokenExpireDuration,
-		refreshTokenExpireDuration: refreshTokenExpireDuration,
+func NewTokenService(client *mongo.Client, idTokenExpireDuration time.Duration) TokenService {
+	return TokenService{
+		UsersCollection:        getCollection(client, UsersCollection),
+		RefreshTokenCollection: getCollection(client, RefreshTokenCollection),
+		idTokenExpireDuration:  idTokenExpireDuration,
 	}
 }
 
@@ -46,27 +48,48 @@ func (service *TokenService) NewIdToken(uid string) (string, error) {
 	return t, nil
 }
 
+type RefreshClaims struct {
+	Uid          string
+	RefreshToken string
+	jwt.RegisteredClaims
+}
+
 // TODO: Create Refresh Token for user
 /*
 	!Errors
 		User doesn't exist
-
 */
 func (service *TokenService) NewRefreshToken(uid string) (string, error) {
-	token := jwt.NewWithClaims(jwt.SigningMethodHS256, UserClaims{
-		Uid: uid,
-		RegisteredClaims: jwt.RegisteredClaims{
-			IssuedAt:  jwt.NewNumericDate(time.Now()),
-			ExpiresAt: jwt.NewNumericDate(time.Now().Add(service.refreshTokenExpireDuration)),
-		},
-	})
-
-	t, err := token.SignedString(Secret)
+	// TODO: Check if user exists
+	exists, err := service.CheckIfUserExists(uid)
 	if err != nil {
-		return "", fmt.Errorf("new id token: %w", err)
+		return "", fmt.Errorf("new refresh token: %w", err)
 	}
 
-	return t, nil
+	if exists {
+		refreshToken, err := rand.String(32)
+		if err != nil {
+			return "", fmt.Errorf("new refresh token: %w", err)
+		}
+
+		token := jwt.NewWithClaims(jwt.SigningMethodHS256, RefreshClaims{
+			Uid:          uid,
+			RefreshToken: refreshToken,
+			RegisteredClaims: jwt.RegisteredClaims{
+				IssuedAt: jwt.NewNumericDate(time.Now()),
+			},
+		})
+
+		t, err := token.SignedString(Secret)
+		if err != nil {
+			return "", fmt.Errorf("new id token: %w", err)
+		}
+
+		return t, nil
+	} else {
+		return "", fmt.Errorf("new refresh token: user doesn't exist")
+	}
+
 }
 
 /*
@@ -75,7 +98,26 @@ User doesn't exist
 Refresh token expired && not valid => refresh token isn't same as the refresh token on DB
 */
 func (service *TokenService) RefreshIdToken(uid, refreshToken string) (string, error) {
-	// TODO: Create random string as
 
 	panic("Implement")
+}
+
+// Returns true if user exists
+func (service *TokenService) CheckIfUserExists(uid string) (bool, error) {
+	objId, err := primitive.ObjectIDFromHex(uid)
+	if err != nil {
+		return false, fmt.Errorf("check if user exists: %w", err)
+	}
+
+	count, err := service.UsersCollection.CountDocuments(context.TODO(), bson.M{
+		"_id": objId,
+	})
+
+	if err != nil {
+		return false, fmt.Errorf("check if user exists: %w", err)
+	}
+
+	fmt.Println("count", count)
+
+	return count > 0, nil
 }
--- setup/services.go
+++ setup/services.go
@@ -1,13 +1,16 @@
 package setup
 
 import (
+	"time"
+
 	"github.com/AkifhanIlgaz/vocab-builder/models"
 )
 
 type services struct {
-	WordService *models.WordService
-	BoxService  *models.BoxService
-	UserService *models.UserService
+	WordService  *models.WordService
+	BoxService   *models.BoxService
+	UserService  *models.UserService
+	TokenService *models.TokenService
 }
 
 func Services(databases *databases) (*services, error) {
@@ -16,13 +19,16 @@ func Services(databases *databases) (*services, error) {
 
 	userService := models.NewUserService(databases.Mongo)
 
+	tokenService := models.NewTokenService(databases.Mongo, 15*time.Minute)
+
 	boxService := models.BoxService{
 		DB: databases.Bolt,
 	}
 
 	return &services{
-		WordService: &wordService,
-		BoxService:  &boxService,
-		UserService: &userService,
+		WordService:  &wordService,
+		BoxService:   &boxService,
+		UserService:  &userService,
+		TokenService: &tokenService,
 	}, nil
 }
