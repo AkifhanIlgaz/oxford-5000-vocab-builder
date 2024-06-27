6a9fe9bbc273f3bb3e2b81f04b1a4b3bd788f928
AkifhanIlgaz <akifhanilgaz@gmail.com>
2024-06-27 22:04:18 +0300

    init config logic

--- main.go
+++ main.go
@@ -1,5 +1,17 @@
 package main
 
-func main() {
+import (
+	"fmt"
 
+	"github.com/AkifhanIlgaz/vocab-builder/config"
+)
+
+func main() {
+	cfg, err := config.Load()
+	if err != nil {
+		fmt.Printf("Error loading config: %v\n", err)
+		return
+	}
+
+	_ = cfg
 }
