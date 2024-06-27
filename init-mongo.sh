0b5a12738621da321d85f99e5b9d6c5dd52bdd87
AkifhanIlgaz <akifhanilgaz@gmail.com>
2024-06-27 20:46:49 +0300

    update

--- init-mongo.sh
+++ init-mongo.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -e
 
-mongoimport --db oxford-5000 --collection words --type json --file /data/oxford-5000.json --jsonArray
+mongoimport --username root --password password123 --authenticationDatabase admin --db oxford-5000 --collection words --type json --file /data/oxford-5000.json --jsonArray
